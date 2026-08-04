f5cf22786ac8a9e0fb56baf6802f852fffed02f6
Rifryx <alexapon36@gmail.com>
2026-08-04 22:07:11 +0300

    register new router

--- backend/app/core/application.py
+++ backend/app/core/application.py
@@ -6,6 +6,8 @@ from app.api.routers import (
 
 def register_routers(app: FastAPI):
     app.include_router(health.router)
+    app.include_router(users.router)
+    
     
 
 def create_app() -> FastAPI:
