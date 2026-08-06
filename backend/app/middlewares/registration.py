4ad1ac4749b44581f55c28884a9bbbd76f07dde2
Rifryx <alexapon36@gmail.com>
2026-08-06 11:20:23 +0300

    обернули в екн учсузе

--- backend/app/middlewares/registration.py
+++ backend/app/middlewares/registration.py
@@ -7,9 +7,17 @@ class RegistrationMiddleware(BaseMiddleware):
         tg_user = data.get("event_from_user")
         if tg_user is None:
             return await handler(event, data)
-        user, is_new = await UserService.get_or_create(tg_id=tg_user.id,
-                                                       username=tg_user.username,
-                                                       first_name=tg_user.first_name)
+        try:
+            user, is_new = await UserService.get_or_create(
+                tg_id=tg_user.id,
+                username=tg_user.username,
+                first_name=tg_user.first_name,
+            )
+        except ValueError:
+            user = await UserService.get_user(tg_user.id)
+            if user is None:
+                raise
+            is_new = False
         data["user"] = user
         data["is_new_user"] = is_new
         return await handler(event, data)
