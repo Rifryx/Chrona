3cca2b81167f44d8d850f1e8808992ccdb8b0441
Rifryx <alexapon36@gmail.com>
2026-08-10 20:13:45 +0300

    add new schedule router

--- backend/app/core/application.py
+++ backend/app/core/application.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api.routers import (
     health, users, tasks, projects,
     planner, ai, settings, categories,
+    schedule
 )
 
 def register_routers(app: FastAPI):
@@ -11,6 +12,7 @@ def register_routers(app: FastAPI):
     app.include_router(categories.router)
     app.include_router(projects.router)
     app.include_router(settings.router)
+    app.include_router(schedule.router)
 
 
 def create_app() -> FastAPI:
